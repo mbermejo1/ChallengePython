fd92545de0a580139369a314a78285d2773e32bc
matias.bermejo.ceb@immune.institute <matias.bermejo.ceb@immune.institute>
2021-11-24 10:57:45 +0100

    challenge1

---
+++ 1.strings/Challenge1_mbermejo.py
@@ -0,0 +1,10 @@
+print("Welcome!")
+name = input("your name?").capitalize()
+
+print("Nice to see you", name)
+print("This is a program that will count the times the letter you choose appears in your message.")
+message = input("Write a message.")
+letter = input("whitch letter would you like to count?")
+
+
+print("The letter you chose appeared", message.count(letter), "times")
